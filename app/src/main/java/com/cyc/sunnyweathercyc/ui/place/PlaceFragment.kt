a126e4b863017b778f5b53bc288dbb9200ebeadf
cyc <381551821@qq.com>
2020-05-15 17:23:01 +0800

    新增切换城市和手动刷新天气的功能。

--- app/src/main/java/com/cyc/sunnyweathercyc/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/cyc/sunnyweathercyc/ui/place/PlaceFragment.kt
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.cyc.sunnyweathercyc.MainActivity
 import com.cyc.sunnyweathercyc.R
 import com.cyc.sunnyweathercyc.ui.weather.WeatherActivity
 import kotlinx.android.synthetic.main.fragment_place.*
@@ -25,7 +26,7 @@ class PlaceFragment: Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if (viewModel.isSavedPlace()){
+        if (activity is MainActivity && viewModel.isSavedPlace()){
             val place = viewModel.getSavedPlace()
             val intent = Intent(context,WeatherActivity::class.java).apply {
                 putExtra("location_lng",place.location.lng)
--- app/src/main/java/com/cyc/sunnyweathercyc/ui/weather/WeatherActivity.kt
+++ app/src/main/java/com/cyc/sunnyweathercyc/ui/weather/WeatherActivity.kt
@@ -1,13 +1,17 @@
 package com.cyc.sunnyweathercyc.ui.weather
 
+import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cyc.sunnyweathercyc.R
@@ -39,6 +43,22 @@ class WeatherActivity : AppCompatActivity() {
             viewModel.placeName = intent.getStringExtra("place_name") ?:""
         }
 
+        navBtn.setOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+        drawerLayout.addDrawerListener(object :DrawerLayout.DrawerListener{
+            override fun onDrawerStateChanged(newState: Int) {}
+
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+
+            override fun onDrawerOpened(drawerView: View) {}
+
+            override fun onDrawerClosed(drawerView: View) {
+                val manager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                manager.hideSoftInputFromWindow(drawerView.windowToken,InputMethodManager.HIDE_NOT_ALWAYS)
+            }
+        })
+
         viewModel.weatherLiveData.observe(this, Observer {
             val weather = it.getOrNull()
             if (weather != null){
@@ -47,8 +67,17 @@ class WeatherActivity : AppCompatActivity() {
                 Toast.makeText(this,"无法成功获取天气信息",Toast.LENGTH_SHORT).show()
                 it.exceptionOrNull() ?.printStackTrace()
             }
+            swipeRefresh.isRefreshing = false
         })
+        swipeRefresh.setColorSchemeResources(R.color.colorPrimary)
+        refreshWeather()
+        swipeRefresh.setOnRefreshListener {
+            refreshWeather()
+        }
+    }
+    fun refreshWeather(){
         viewModel.refreshWeather(viewModel.locationLng,viewModel.locationLat)
+        swipeRefresh.isRefreshing = true
     }
     private fun showWeatherInfo(weather: Weather){
         placeName.text = viewModel.placeName
